4a07db055f7d83fcc8713feb2775bb6b8755a4ab
Massimo Tascone <tascone.massimo@gmail.com>
2020-01-29 14:43:34 +0100

    Updates

---
+++ precache-manifest.3b592a68e3eac0a174d4334b0f1a5d6c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c163afe2182913b031c6be4111218699",
+    "url": "/react-axios-reddit/index.html"
+  },
+  {
+    "revision": "cc3c9853db0d8efcb371",
+    "url": "/react-axios-reddit/static/css/main.11921e70.chunk.css"
+  },
+  {
+    "revision": "183a753715fb30a0489b",
+    "url": "/react-axios-reddit/static/js/2.8487fa21.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-axios-reddit/static/js/2.8487fa21.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cc3c9853db0d8efcb371",
+    "url": "/react-axios-reddit/static/js/main.6f3c0c49.chunk.js"
+  },
+  {
+    "revision": "b22cf2115a8f2957bae2",
+    "url": "/react-axios-reddit/static/js/runtime-main.5a7164f9.js"
+  }
+]);
